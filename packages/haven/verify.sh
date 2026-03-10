555bacd0966a1f1ebd5be2c0d816330d387ae0a4
tinfoil hash <238757206+tinfoilhash@users.noreply.github.com>
2026-03-10 06:06:06 -0400

    add haven (#1)

---
+++ packages/haven/verify.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+package_dir=$(dirname -- "$(realpath "$0")")
+cd "$package_dir"
+
+source ../../common/github.sh
+
+check_dependencies
+
+echo '--- verifying haven'
+
+package_name='haven'
+repo='barrydeen/haven'
+key_fingerprint='19243581B019B2452DA2F82870FF859890221E23' # https://raw.githubusercontent.com/barrydeen/haven/master/haven.asc
+shasum_filename_pattern='checksums.txt'
+shasum_signature_filename_pattern="$shasum_filename_pattern.sig"
+
+verify_from_github "$package_name" "$repo" "$key_fingerprint" "$shasum_filename_pattern" "$shasum_signature_filename_pattern"
