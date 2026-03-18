ee99cf6ad4821d9c40d7836f2700299314d46de9
tinfoil hash <238757206+tinfoilhash@users.noreply.github.com>
2026-03-18 06:04:17 -0400

    add eclair (#8)

---
+++ packages/eclair/verify.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+package_dir=$(dirname -- "$(realpath "$0")")
+cd "$package_dir"
+
+source ../../common/github.sh
+
+check_dependencies
+
+echo '--- verifying eclair'
+
+package_name='eclair'
+repo='ACINQ/eclair'
+key_fingerprints=(
+  '55092314AD0547C59BDB7AE8E04E48E72C205463' # https://acinq.co/pgp/drouinf2.asc
+)
+shasum_filename_pattern='SHA256SUMS.asc'
+shasum_signature_filename_pattern="$shasum_filename_pattern"
+
+verify_from_github "$package_name" "$repo" "$key_fingerprints" "$shasum_filename_pattern" "$shasum_signature_filename_pattern"
